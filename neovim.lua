39fbe7394f3438f0d31f10171582edaa6446ff26
Amit <theamit.969@gmail.com>
2025-09-11 10:18:55 +0530

    closest colorscheme for this theme

--- neovim.lua
+++ neovim.lua
@@ -1,12 +1,8 @@
 return {
 	{
-		"hachy/eva01.vim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("eva01")
-			-- or
-			-- vim.cmd.colorscheme "eva01-LCL"
-		end,
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
 	},
 }
